23eb56160f51f17d0fe41ef1c70d2ab6979565fd
md-sakib-softvence <sakibsoftvence@gmail.com>
2025-11-15 17:04:37 +0600

    chage the manager and employ

--- src/modules/project/controller/project.controller.ts
+++ src/modules/project/controller/project.controller.ts
@@ -12,14 +12,18 @@ import { CreateProjectDto } from '../dto/create-project.dto';
 import { FindAllProjectsDto } from '../dto/find-all-projects.dto';
 import { SearchProjectByNameDto } from '../dto/search-project.dto';
 import { JwtAuthGuard } from 'src/common/jwt/jwt.guard';
+import { RolesGuard } from 'src/common/jwt/roles.guard';
+import { Roles } from 'src/common/jwt/roles.decorator';
+import { Role } from 'generated/prisma';
 
 @Controller('project')
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard,RolesGuard)
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   //  Create a new project
   @Post()
+  @Roles(Role.CLIENT)
   async create(@Body() createProjectDto: CreateProjectDto) {
     const newProject = await this.projectService.create(createProjectDto);
     return {
