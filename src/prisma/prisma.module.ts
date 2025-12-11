fbb2b2edb9f22f5b257d93f17248c5fc7ed7afbe
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-12-11 15:19:43 +0600

    chage

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-
+console.log('Using @prisma/client version:', require('@prisma/client/package.json').version);
 @Global()
 @Module({
   providers: [PrismaService],
