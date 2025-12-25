555aa8eedbf7a59694495950264abb6431ecc8a4
SouravChowdhuryRimel <souravchowdhury6519@gmail.com>
2025-12-25 11:42:19 +0600

    update viewer prisma model

--- src/modules/user/dto/create-viewer.dto.ts
+++ src/modules/user/dto/create-viewer.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsArray, IsString } from 'class-validator';
 import { BaseUserDto } from './base-user.dto';
 
 export class CreateViewerDto extends BaseUserDto {
-  // @ApiProperty({ example: 'your-client-id' })
-  // @IsString()
-  // clientId: string;
+  @ApiProperty({
+    example: ['project-id-1', 'project-id-2'],
+    description: 'Array of project IDs associated with the viewer',
+    type: [String],
+  })
+  @IsArray()
+  @IsString({ each: true })
+  projectId: string[];
 }
