f326a00cadc257c6f438715d781cccdc92442145
Tirtho-Ray <tirthoray10@gmail.com>
2026-02-15 09:12:28 +0600

    update crerate employee

--- src/modules/feedbackAndSupport/controller/FS.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { FSService } from "../service/FS.service";
-
-@Controller('feedback')
-export class FSController {
-    constructor(private readonly service:FSService) {}
-}
